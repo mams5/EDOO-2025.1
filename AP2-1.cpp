0771500390120567b74d960fa134e6f5d8c0edbe
Maria Antônia <131059210+mams5@users.noreply.github.com>
2025-05-14 12:46:43 -0300

    EDOO's AP2-1.
    Atividade AP2.1 da segunda parte de Estruturas de Dados Orientadas a Objeto.

---
+++ AP2-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    
+    float num;
+    cout<<"Insira um número: ";
+    cin>>num;
+    float squareRoot;
+    squareRoot = sqrt(num);
+    cout<<"A raíz quadrada de "<< num<< " é "<<squareRoot<<endl;
+
+    return 0;
+    
+}
